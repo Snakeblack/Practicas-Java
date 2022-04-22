bc5f017add7c0ac49d1353c020305b902553654f
sn4ke-laptop <michael.retamozo@gmail.com>
2022-04-22 16:27:59 +0200

    add: ejercicio Polymers

---
+++ M1UD-04-EJ-4-Polymers/src/ejercicio/Polymers.java
@@ -0,0 +1,30 @@
+package ejercicio;
+
+public class Polymers {
+    /**
+     * Un polímero es una macromolécula formada por la unión de una o más unidades
+     * simples llamadas monómeros. En nuestro laboratorio identificamos los monómeros
+     * con letras del alfabeto, de la "A" a la "Z" (sin contar la "Ñ". Si la letra está
+     * en mayúsculas representa un monómero con polaridad positiva, si está en mayúsculas
+     * representa polaridad negativa.
+     *
+     * Cuando en un polímero coinciden dos unidades con polaridades distintas, ambas unidades
+     * reaccionan destruyéndose y dejando al polímero sin las mismas. Por ejemplo, en el
+     * polímero "CaAdbb", el par "aA" reacciona autoeliminándose y dejándo el polímero como
+     * "Cdbb". "C" y "d" son de polaridades distintas pero al ser distintos monómeros no
+     * reaccionan entre sí. Las unidades "bb" son del mismo tipo pero al tener la misma
+     * polaridad no reacionan entre sí.
+     *
+     * La eliminación de unidades o monómeros puede producir eliminaciones en cascada. Por
+     * ejemplo, el polímero "rFvdAaDVb" al reaccionar elimina sucesivamente el par "Aa", el
+     * par "dD" y el par "vV" para dejar un resultado final de "rFb" Una reacción en cadena
+     * puede eliminar por completo el polímero.
+     *
+     * Escribe una función que dado un polímero representado con las letras del alfabeto
+     * (mayúsculas y minúsculas excluyendo la Ñ), procesa las reacciones en cadena de dicho
+     * polímero y devuelve el polímero reducido.
+     */
+    public static String processPolymer(String polymer) {
+        /* ... */
+    }
+}
