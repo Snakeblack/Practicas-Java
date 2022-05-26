3465e6fe4e2637e467f77b52ed8ceb7142dbf73a
sn4ke-laptop <michael.retamozo@gmail.com>
2022-05-26 15:52:32 +0200

    add: Ejercicio Cartas

---
+++ M1_UD-06-EJ-4-Cartas/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Baraja baraja = new Baraja();
+        Mazo mazo = baraja.getMazo();
+        System.out.println(mazo.toString());
+
+        mazo.setAlgoritmo(new OrdPalNumInc());
+        mazo.ordena();
+        System.out.println(mazo.toString());
+
+        mazo.setAlgoritmo(new OrdPalNumDec());
+        mazo.ordena();
+        System.out.println(mazo.toString());
+
+        mazo.setAlgoritmo(new OrdNumIncPal());
+        mazo.ordena();
+        System.out.println(mazo.toString());
+    }
+}
