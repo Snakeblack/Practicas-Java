d820aae7a52ecb335e26713a9876a60b5331b6ca
sn4ke-desktop <michael.retamozo@gmail.com>
2022-06-21 04:03:31 +0200

    update: ejercicio Cartas Pulido

--- M1_UD-06-EJ-4-Cartas/src/OrdPalNumDec.java
+++ M1_UD-06-EJ-4-Cartas/src/OrdPalNumDec.java
@@ -10,7 +10,7 @@ public class OrdPalNumDec extends AlgoritmoOrdenacion {
         String[] numerosBaraja = Baraja.numeros;
         String[] palosBaraja = Baraja.palos;
         String[] numerosInvertidos = new String[numerosBaraja.length];
-        
+
         Arrays.sort(palosBaraja, String.CASE_INSENSITIVE_ORDER);
 
         int cont = 0;
@@ -18,32 +18,24 @@ public class OrdPalNumDec extends AlgoritmoOrdenacion {
             numerosInvertidos[cont] = numerosBaraja[i];
             cont++;
         }
-        
-        for (String palo : palosBaraja) {
-            for (String numero : numerosInvertidos) {
-                listaOrdenada.add(new Carta(numero, palo));
-            }
-        }
-        
-        for (int i = 0; i < listaOrdenada.size(); i++) {
-            for (int j = 0; j < listaCartas.size(); j++) {
-                int resultado = comparadorCarta((Carta) listaOrdenada.get(i),(Carta) listaCartas.get(j));
 
-                if(resultado == 1){ 
+        for (String palo : palosBaraja)
+            for (String numero : numerosInvertidos)
+                listaOrdenada.add(new Carta(numero, palo));
+
+        for (int i = 0; i < listaOrdenada.size(); i++)
+            for (int j = 0; j < listaCartas.size(); j++)
+                if (comparadorCarta((Carta) listaOrdenada.get(i), (Carta) listaCartas.get(j)) == 1)
                     mazoOrdenado.add(listaOrdenada.get(i));
-                }
-            }
-        }
+
         listaCartas.clear();
         listaCartas.addAll(mazoOrdenado);
     }
-    
 
     public static int comparadorCarta(Carta c1, Carta c2) {
-        if (c1.getNumero().equals(c2.getNumero()) && c1.getPalo().equals(c2.getPalo())) {
+        if (c1.getNumero().equals(c2.getNumero()) && c1.getPalo().equals(c2.getPalo()))
             return 1;
-        } else {
+        else
             return 0;
-        }
     }
 }
--- M1_UD-06-EJ-4-Cartas/src/OrdPalNumInc.java
+++ M1_UD-06-EJ-4-Cartas/src/OrdPalNumInc.java
@@ -9,34 +9,26 @@ public class OrdPalNumInc extends AlgoritmoOrdenacion {
         List mazoOrdenado = new ArrayList();
         String[] numerosBaraja = Baraja.numeros;
         String[] palosBaraja = Baraja.palos;
-        
-        Arrays.sort(palosBaraja, String.CASE_INSENSITIVE_ORDER);
-        
-        for (String palo : palosBaraja) {
-            for (String numero : numerosBaraja) {
-                listaOrdenada.add(new Carta(numero, palo));
-            }
-        }
-        
-        for (int i = 0; i < listaOrdenada.size(); i++) {
-            for (int j = 0; j < listaCartas.size(); j++) {
-                int resultado = comparadorCarta((Carta) listaOrdenada.get(i),(Carta) listaCartas.get(j));
 
-                if(resultado == 1){ 
+        Arrays.sort(palosBaraja, String.CASE_INSENSITIVE_ORDER);
+
+        for (String palo : palosBaraja)
+            for (String numero : numerosBaraja)
+                listaOrdenada.add(new Carta(numero, palo));
+
+        for (int i = 0; i < listaOrdenada.size(); i++)
+            for (int j = 0; j < listaCartas.size(); j++)
+                if (comparadorCarta((Carta) listaOrdenada.get(i), (Carta) listaCartas.get(j)) == 1)
                     mazoOrdenado.add(listaOrdenada.get(i));
-                }
-            }
-        }
+
         listaCartas.clear();
         listaCartas.addAll(mazoOrdenado);
     }
-    
 
     public static int comparadorCarta(Carta c1, Carta c2) {
-        if (c1.getNumero().equals(c2.getNumero()) && c1.getPalo().equals(c2.getPalo())) {
+        if (c1.getNumero().equals(c2.getNumero()) && c1.getPalo().equals(c2.getPalo()))
             return 1;
-        } else {
+        else
             return 0;
-        }
     }
 }
