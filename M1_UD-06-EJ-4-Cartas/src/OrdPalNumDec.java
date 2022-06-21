c468379ab846aaa8869f99c360b87ccb1f60339e
sn4ke-desktop <michael.retamozo@gmail.com>
2022-06-21 04:22:34 +0200

    update: ejercicio Carta Comentado

--- M1_UD-06-EJ-4-Cartas/src/OrdPalNumDec.java
+++ M1_UD-06-EJ-4-Cartas/src/OrdPalNumDec.java
@@ -11,27 +11,35 @@ public class OrdPalNumDec extends AlgoritmoOrdenacion {
         String[] palosBaraja = Baraja.palos;
         String[] numerosInvertidos = new String[numerosBaraja.length];
 
+        // Ordeno los palos de la baraja de forma alfabetica
         Arrays.sort(palosBaraja, String.CASE_INSENSITIVE_ORDER);
 
+        // Invierto los numeros de la baraja para que sean de forma decreciente
         int cont = 0;
         for (int i = numerosBaraja.length - 1; i >= 0; i--) {
             numerosInvertidos[cont] = numerosBaraja[i];
             cont++;
         }
 
+        // Creo una lista con objetos Carta con los numeros y palos de la baraja
         for (String palo : palosBaraja)
             for (String numero : numerosInvertidos)
                 listaOrdenada.add(new Carta(numero, palo));
 
+        // Recorro la lista ordenada creada previamente y comparo con la lista de cartas que me pasan como parametro
+        // Si la carta de la lista de parametro esta en la lista ordenada, la añado a la lista de ordenada
+        // La comparacion se hace con la funcion comparadorCarta();
         for (int i = 0; i < listaOrdenada.size(); i++)
             for (int j = 0; j < listaCartas.size(); j++)
                 if (comparadorCarta((Carta) listaOrdenada.get(i), (Carta) listaCartas.get(j)) == 1)
                     mazoOrdenado.add(listaOrdenada.get(i));
 
+        //Elimino los elementos de la lista y la reemplazo por la lista ordenada
         listaCartas.clear();
         listaCartas.addAll(mazoOrdenado);
     }
 
+    // Funcion que compara dos cartas y devuelve un numero 1 si son iguales.
     public static int comparadorCarta(Carta c1, Carta c2) {
         if (c1.getNumero().equals(c2.getNumero()) && c1.getPalo().equals(c2.getPalo()))
             return 1;
